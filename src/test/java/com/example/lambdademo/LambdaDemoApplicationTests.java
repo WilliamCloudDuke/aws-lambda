d1872c8d0baff848ee221cafe6db842ca03155e5
WilliamCloudDuke <william.chaparro.salazar@gmail.com>
2024-04-28 12:06:57 -0500

    initial commit - serverless funcion that uses Spring cloud features

---
+++ src/test/java/com/example/lambdademo/LambdaDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lambdademo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LambdaDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
